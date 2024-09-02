c8496329b9f22ef6e7bc45c6579d3ce9639e2fe4
unknown <kennataddese6@gmail.com>
2024-09-02 12:16:33 -0700

    add overlay

--- app/page.tsx
+++ app/page.tsx
@@ -8,16 +8,18 @@ export default function Home() {
           <source src="/hero-video.mp4" type="video/mp4" />
         </video>
         <div className="hero-text-container">
-          <h1 className="text-white text-center">
-            Explore Dubai in Style with Our Premium Cars
-          </h1>
-          <p className="text-white text-center">
-            Choose from a wide range of luxury, sports, and economy vehicles to
-            suit your needs. Enjoy competitive rates, flexible options, and
-            exceptional service. Book today and explore the city’s iconic
-            landmarks in style and comfort.
-          </p>
-          <button className="mx-auto d-block hero-button">Get started</button>
+          <div>
+            <h1 className="text-white text-center">
+              Explore Dubai in Style with Our Premium Cars
+            </h1>
+            <p className="text-white text-center">
+              Choose from a wide range of luxury, sports, and economy vehicles
+              to suit your needs. Enjoy competitive rates, flexible options, and
+              exceptional service. Book today and explore the city’s iconic
+              landmarks in style and comfort.
+            </p>
+            <button className="mx-auto d-block hero-button">Get started</button>
+          </div>
         </div>
       </div>
     </>
